078d940e76e81acbbb09ab1b40c43fc016090827
Ana Júlia Gusmão <129332966+anajugn@users.noreply.github.com>
2023-11-24 13:12:08 -0300

    Update Professor.java

--- Trabalho_POO/src/entidades/Professor.java
+++ Trabalho_POO/src/entidades/Professor.java
@@ -3,6 +3,26 @@ package entidades;
 public class Professor {
 	
 	private int id;
+	private String nome;
 	
-	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 }
