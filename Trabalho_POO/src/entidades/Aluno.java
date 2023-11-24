f5bdb116e44ce6a0f23bfb26082c3750ca813625
Ana Júlia Gusmão <129332966+anajugn@users.noreply.github.com>
2023-11-24 13:13:26 -0300

    Update Aluno.java

--- Trabalho_POO/src/entidades/Aluno.java
+++ Trabalho_POO/src/entidades/Aluno.java
@@ -1,5 +1,38 @@
 package entidades;
 
 public class Aluno {
-
+private int id;
+	private int matricula;
+	private String nome;
+	private boolean definicao;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public boolean isDefinicao() {
+		return definicao;
+	}
+	public void setDefinicao(boolean definicao) {
+		this.definicao = definicao;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 }
