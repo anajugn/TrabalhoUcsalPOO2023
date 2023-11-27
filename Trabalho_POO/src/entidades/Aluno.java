7d5639c8d7bc7b81d9248497c4e4e9f264c08937
Maria Fernanda Prandi <127303256+Mafeepam@users.noreply.github.com>
2023-11-27 07:26:15 -0300

    Update Aluno.java

--- Trabalho_POO/src/entidades/Aluno.java
+++ Trabalho_POO/src/entidades/Aluno.java
@@ -1,5 +1,35 @@
 package entidades;
 
 public class Aluno {
+  
+  	private Integer id;
+	private Integer matricula;
+	private String nome;
+	private String Status;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status;
+	}
+	
 
 }
