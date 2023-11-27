21038d022c36ac215a5726b9d5829b2465ddbe28
Maria Fernanda Prandi <127303256+Mafeepam@users.noreply.github.com>
2023-11-27 09:34:34 -0300

    Update IBase.java

--- Trabalho_POO/src/programa/IBase.java
+++ Trabalho_POO/src/programa/IBase.java
@@ -1,53 +1,13 @@
 package programa;
 
-import java.util.Iterator;
+import java.util.List;
 
-public class IBase {
+public interface IBase {
 	
-	private final Integer
-	
-			QTDE_DEPARTAMENTOS = 3,
-			QTDE_PROFESSORES = 15,
-			QTDE_LABORATORIOS = 12;
-
-
-	List<Laboratorio> getLaboratorios();
+	List<Laboratorio> getlaboratorios();
 	List<Departamento> getDepartamentos();
 	List<Disciplina> getDisciplina();
 	List<Professor> getProfessores();
 	List<Aluno> getAlunos(int qtde, int inicio);
-	
-	private void labortatorios() {
-		this.laboratorios() = new ArrayList<Laboratorio>();
-		Laboratorio lab;
-		
-		for (int i = 0; i <	QTDE_LABORATORIOS; i++, ctle++) {
-			lab.setId(Long.valueOf(ctle));
-			lab.setDescricao("LAMI"+ ctle);
-			
-		}
-			
-		}
-	
-	private void professores() {
-		this.professores() = new ArrayList<Professor>();
-		Professor prof;
-		
-		for (int i = 0; i < QTDE_PROFESSORES; i++, ctle++) {
-			prof.setId(Long.valueOf(ctle));
-			prof.setDescricao("LAMI" + ctle);
-			
-		}
-	}
-	
-	private void departamentos() {
-		this.departamentos() = new ArrayList<Departamento>();
-		Departamento dep;
-		
-		for (int i = 0; i < QTDE_DEPARTAMENTOS; i++, ctle++) {
-			dep.setId(Long.valueOf(ctle));
-			dep.SetDescricao("LAMI" + ctle);
-			
-		}
-	}
-	}
+
+}
