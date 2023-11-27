68e1decf6af45f041a52f12f0ddd9a7d4b0489c5
Maria Fernanda Prandi <127303256+Mafeepam@users.noreply.github.com>
2023-11-27 07:31:39 -0300

    Update Laboratório.java

--- Trabalho_POO/src/entidades/Laboratório.java
+++ Trabalho_POO/src/entidades/Laboratório.java
@@ -2,11 +2,44 @@ package entidades;
 
 public class Laboratório {
 	
-	private int id;
+		private Integer id;
 	private int capacidade;
 	private int maquina;
-	private boolean estado;
+	private boolean status;
 	
+	
+
+	public int getMaquina() {
+		return maquina;
+	}
+
+	public void setMaquina(int maquina) {
+		this.maquina = maquina;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public int getCapacidade() {
+		return capacidade;
+	}
+
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	
 	
 }
