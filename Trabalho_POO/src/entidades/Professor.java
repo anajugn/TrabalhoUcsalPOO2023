583b68776325f005c498dc81dcf2a61349cd98d2
Maria Fernanda Prandi <127303256+Mafeepam@users.noreply.github.com>
2023-11-27 07:26:52 -0300

    Update Professor.java

--- Trabalho_POO/src/entidades/Professor.java
+++ Trabalho_POO/src/entidades/Professor.java
@@ -2,7 +2,35 @@ package entidades;
 
 public class Professor {
 	
-	private int id;
+	private Integer id;
+	private String nome;
+	private String status;
+	private Departamento departamento;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
 	
 	
 }
